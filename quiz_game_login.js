aff43f35f74afee6d940b209e8356d0412084159
Pearl <143796216+PearlKaur@users.noreply.github.com>
2024-06-28 16:11:07 +1000

    Add files via upload
    RE-upLOADED!

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -1,10 +1,11 @@
 addUser()
 {
+    window.location = "quiz_game_page.html"
+    
     player1_name = document.getElementById("player1_name").value
     player2_name = document.getElementById("player2_name").value
 
     localStorage.setItem("player1_name", player1_name);
     localStorage.setItem("player2_name", player2_name);
 
-    window.location = "quiz_game_page.html"
 }
